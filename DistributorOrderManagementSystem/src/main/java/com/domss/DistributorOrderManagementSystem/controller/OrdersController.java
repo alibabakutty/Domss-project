1204a122459817109545fa3dac9f3b99c05f5756
Siva-Sundhar <tdlsiva03@gmail.com>
2024-07-05 17:12:55 +0530

    OrdersService OrdersRepository ReportDto are added

--- DistributorOrderManagementSystem/src/main/java/com/domss/DistributorOrderManagementSystem/controller/OrdersController.java
+++ DistributorOrderManagementSystem/src/main/java/com/domss/DistributorOrderManagementSystem/controller/OrdersController.java
@@ -1,4 +1,45 @@
 package com.domss.DistributorOrderManagementSystem.controller;
 
+
+import com.domss.DistributorOrderManagementSystem.dto.ReportDto;
+import com.domss.DistributorOrderManagementSystem.entity.Orders;
+import com.domss.DistributorOrderManagementSystem.service.OrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("orders")
 public class OrdersController {
+
+    @Autowired
+    private OrdersService orderService;
+
+    @PostMapping("/booking")
+    public ResponseEntity<String> saveOrder(@RequestBody Orders order){
+        return orderService.addOrder(order);
+    }
+    @GetMapping("/getAllOrders")
+    public ResponseEntity<List<Orders>> getAllOrders(){
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/getOrder/{orderId}")
+    public ResponseEntity<Orders> findOrderById(@PathVariable Long orderId){
+        return orderService.findOrderById(orderId);
+    }
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<String> updateOrder(@RequestBody Orders order, @PathVariable Long orderId){
+        return orderService.updateOrder(order, orderId);
+    }
+
+    @GetMapping("/getInformation")
+    public List<ReportDto> getJoinInformation(){
+        return orderService.getJoinInformation();
+    }
+
 }
--- DistributorOrderManagementSystem/src/main/java/com/domss/DistributorOrderManagementSystem/entity/OrderItems.java
+++ DistributorOrderManagementSystem/src/main/java/com/domss/DistributorOrderManagementSystem/entity/OrderItems.java
@@ -1,7 +1,6 @@
 package com.domss.DistributorOrderManagementSystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +11,43 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItems {
+    @Id
+    @SequenceGenerator(
+            name = "order_item_seq",
+            sequenceName = "order_item_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "order_item_seq"
+    )
+    @Column(name = "order_item")
+    private Long Id;
 
+    @Column(name = "stock_category")
+    private String category;
+
+    @Column(name = "product_code")
+    private String code;
+
+    @Column(name = "stock_item_code")
+    private String description;
+
+    @Column(name = "order_quantity")
+    private Double orderQty;
+
+    @Column(name = "units_of_measurement")
+    private String uom;
+
+    @Column(name = "approved_quantity")
+    private Double approvedQuantity;
+
+    @Column(name = "rate")
+    private Double rate;
+
+    @Column(name = "discount")
+    private Double discount;
+
+    @Column(name = "amount")
+    private Double amount;
 }
