e808a988e24c0a0b2dd7aa559e2550874ded1dbc
Siva-Sundhar <tdlsiva03@gmail.com>
2024-07-05 16:42:09 +0530

    first commit

--- DistributorOrderManagementSystem/src/main/java/com/domss/DistributorOrderManagementSystem/exception/CustomException.java
+++ DistributorOrderManagementSystem/src/main/java/com/domss/DistributorOrderManagementSystem/exception/CustomException.java
@@ -1,7 +1,5 @@
 package com.domss.DistributorOrderManagementSystem.exception;
 
 public class CustomException extends Exception{
-    public static void main(String[] args) {
-        System.out.println();
-    }
+
 }
