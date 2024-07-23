8d44703108d91915cef15efbfca1756139360b8b
Siva-Sundhar <tdlsiva03@gmail.com>
2024-07-23 10:41:40 +0530

    basic changes

--- DistributorOrderManagementSystem-frontend/src/components/create/MasterForm.jsx
+++ DistributorOrderManagementSystem-frontend/src/components/create/MasterForm.jsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom'
-import VoucherTypeMaster from '../../pages/master/voucher_type_master/VoucherTypeMaster'
+// import VoucherTypeMaster from '../../pages/master/voucher_type_master/VoucherTypeMaster'
 import LedgerMaster from '../../pages/master/ledger_master/LedgerMaster'
 import RegionMaster from '../../pages/master/region_master/RegionMaster'
 import ExecutiveMaster from '../../pages/master/executive_master/ExecutiveMaster'
 import DistributorMaster from '../../pages/master/distributor_master/DistributorMaster'
 import ProductMaster from '../../pages/master/product_master/ProductMaster'
 import GodownMaster from '../../pages/master/godown_master/GodownMaster'
+import VoucherTypeCreate from '../../pages/master/voucher_type_master/VoucherTypeCreate'
 
 
 
@@ -16,7 +17,7 @@ const MasterForm = () => {
     const renderComp = () => {
         switch(type){
             case 'voucherType':
-                return <VoucherTypeMaster />
+                return <VoucherTypeCreate />
             case 'ledger':
                 return <LedgerMaster />
             case 'region':
